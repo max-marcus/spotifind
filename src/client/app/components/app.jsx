aa3dc2cf4e3c4d118c871702ec0a2eacb388a3b6
listentomm <maxmarcususc@gmail.com>
2016-12-19 17:03:32 -0800

    audio preview on click working

--- src/client/app/components/app.jsx
+++ src/client/app/components/app.jsx
@@ -8,20 +8,44 @@ class App extends React.Component {
     super(props);
     this.state = {
       artistName: '',
-      trackInfo: []
+      trackInfo: [],
+      audioUrl: null
     };
 
     this.newSearch = this.newSearch.bind(this);
     this.newArtist = this.newArtist.bind(this);
+    this.playPreview = this.playPreview.bind(this);
   }
 
   newArtist(event) {
-    this.setState({artistName: event.target.value});
+    this.setState({ artistName: event.target.value });
   }
 
   newSearch(event) {
     event.preventDefault();
     const tracks = this.getTrackArray(this.state.artistName);
+  }
+
+  playPreview(url) {
+    const audioObject = new Audio(url);
+    if (this.state.audioUrl === null) {
+      audioObject.play();
+      this.setState({ audioUrl: url })
+    }
+    else if (this.state.audioUrl === url) {
+      audioObject.pause();
+    }
+    // else if (this.state.audio ===)
+    audioObject.addEventListener('ended', () => {
+      console.log(this.state.audioUrl);
+      this.setState({ audioUrl: null })
+      console.log('ended: ', this.state.audioUrl);
+    });
+    audioObject.addEventListener('pause', () => {
+      this.setState({ audioUrl: null })
+      console.log('paused: ', this.state.audioUrl);
+    });
+    
   }
 
   getTrackArray(artistName) {
@@ -32,7 +56,7 @@ class App extends React.Component {
           .then(res => {
             const trackArray = res.data.tracks;
             const data = this.convertToUsableData(trackArray);
-            this.setState({trackInfo: data});
+            this.setState({ trackInfo: data });
           })
       })
   }
@@ -52,15 +76,16 @@ class App extends React.Component {
   render() {
     const { trackInfo } = this.state;
     const tracks = trackInfo.map((track, i) => (
-      <Preview key={i} imageUrl={track.imageUrl} spotUrl={track.spotUrl} name={track.name} previewUrl={track.preview}/>
+      <Preview key={i} imageUrl={track.imageUrl} spotUrl={track.spotUrl} name={track.name} previewUrl={track.preview} playPreview={this.playPreview} />
     ));
 
     return (
       <div id="main">
         <form id="search-form" onSubmit={this.newSearch}>
           <label>
-            Search for an artist:
-            <input type="text" value={this.state.artistName} onChange={this.newArtist} />
+            SEARCH FOR AN ARTIST:
+            <br/>
+            <input id="search-box" type="text" value={this.state.artistName} onChange={this.newArtist} />
           </label>
           <input type="submit" value="Search" />
         </form>
--- src/client/app/components/preview.jsx
+++ src/client/app/components/preview.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 const Preview = (props) => {
-  const { imageUrl, spotUrl, name, previewUrl } = props;
+  const { imageUrl, spotUrl, name, previewUrl, playPreview } = props;
 
   return (
-    <div className="track-container">
+    <div className="track-container" onClick={() => {playPreview(previewUrl)}} >
       {name}
-      <img id="art" src={imageUrl} />
+      <img id="art" src={imageUrl} alt="album art" />
       <a id="spotlink" href={spotUrl}>Listen to full track on Spotify</a>
     </div>
   )
